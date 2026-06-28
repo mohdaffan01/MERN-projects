6b83643449fddc712c95b941140ffc725c52a407
affan <mohdaffan338@gmail.com>
2026-06-29 00:32:04 +0530

    add api

--- admin-dashboard/frontend/src/api/axios.js
+++ admin-dashboard/frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true
-  
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
